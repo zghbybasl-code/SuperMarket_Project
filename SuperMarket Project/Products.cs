d8b7c8445d1cfbfc81e3842b48b6285c825051e3
Basil <zghbybasl@gmail.com>
2026-04-21 18:17:15 +0200

    Solution Proplem

--- SuperMarket Project/Products.cs
+++ SuperMarket Project/Products.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace SuperMarket_Project
+{
+    internal class Products
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
